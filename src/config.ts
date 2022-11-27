ed51fe09cef4599e478e90a6c4a40b67c8b68c24
Hammad Ali <hammad.a22002@gmail.com>
2022-11-27 12:48:56 +0500

    server change made from here

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //sever url
-export const BASE_URL = "https://cuireview.herokuapp.com/api/accountancy/";
-// export const BASE_URL = "http://localhost:5000/api/accountancy/";
+// export const BASE_URL = "https://cuireview.herokuapp.com/api/accountancy/";
+export const BASE_URL = "http://localhost:5000/api/accountancy/";
